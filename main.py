84a635dd36929b85bc88b38f23fc096d2593dfe5
Tanjilur Rahaman Ratul <78961785+Evilxploit@users.noreply.github.com>
2021-12-03 23:30:57 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,145 @@
+#coded by Mr.EvilXploit
+#Don't Copy My tool'
+#Advance Tnxxx
+import os,time,pyfiglet
+os.system("clear")
+time.sleep(0.1)
+print("\033[93m"+"*"*49)
+print("")
+print("\033[92m" + """
+██████╗  █████╗ ███████╗██╗ ██████╗    ██████╗ ██╗  ██╗ ██████╗ 
+██╔══██╗██╔══██╗██╔════╝██║██╔════╝    ██╔══██╗██║ ██╔╝██╔════╝ 
+██████╔╝███████║███████╗██║██║         ██████╔╝█████╔╝ ██║  ███╗
+██╔══██╗██╔══██║╚════██║██║██║         ██╔═══╝ ██╔═██╗ ██║   ██║
+██████╔╝██║  ██║███████║██║╚██████╗    ██║     ██║  ██╗╚██████╔╝
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝ ╚═════╝    ╚═╝     ╚═╝  ╚═╝ ╚═════╝ 
+                                                                
+""")
+print("")
+print("\033[92m"+"""
+Coder : Mr.EvilXploit
+
+Facebook : https://www.facebook.com/Mr.EvilXploit
+
+TG :  t.me/EvilXploit
+""")
+print("")
+print("")
+print("\033[93m"+"*"*49)
+print("")
+print("")
+time.sleep(0.3)
+print("\033[92m"+'''
+1. pkg update
+2. pkg update
+3. pkg upgrade
+4. apt install python
+5. apt install git
+6. apt install python2
+7. pip install pyfiglet
+8. apt install php
+9. termux-setup-storage
+10. apt install php
+11. apt install golang
+12. apt install host
+13. apt install nano
+14. apt install cmatrix
+15. apt install figlet
+16. apt install wget
+17. apt install toilet
+18. apt insatll cowsay
+19. apt insatll ruby
+20. gem install lolcat
+21. apt install curl
+22. apt install unzip
+23. apt install openssh
+24. apt install tor
+25.apt install net-tools
+26. apt install unrar
+27. apt install w3m
+28. apt install proot
+29. pip install requests
+30. pip install  --upgrade pip
+31. pip2 install  --upgrade pip
+32. pip install future
+33. pip install youtube-dl
+34. pkg install weechat-python-plugin
+35. pkg install python2-static
+36. pkg install python-tkinter
+37. pkg install vim-python
+38. pkg install python-static
+''')
+print("")
+print("\033[91m"+"All pkg that will be installed through this tool")
+print("")
+print("")
+inp = input("\033[95m"+"ENTER YOUR CHOICE (Y/N): ")
+
+if inp =="Y" :
+  
+  os.system("clear")
+  print("\033[92m"+"Hello Newbes....!!!!!! ")
+  time.sleep(2)
+  print("\033[92m"+"Wait For All Package Download. . . . . . . . . . .")
+  print("")
+  print("")
+  os.system("pkg update -y")
+  os.system("pkg upgrade -y")
+  os.system("apt install python -y")
+  os.system("pip install pyfiglet")
+  os.system("termux-setup-storage")
+  os.system("apt install php -y")
+  os.system("apt install git -y")
+  os.system("apt install golang -y")
+  os.system("apt install host -y")
+  os.system("apt install nano -y")
+  os.system("apt install cmatrix -y")
+  os.system("apt install figlet -y")
+  os.system("apt install wget -y")
+  os.system("apt install toilet -y")
+  os.system("apt install cowsay -y")
+  os.system("apt install ruby -y")
+  os.system("gem install lolcat")
+  os.system("apt install curl -y")
+  os.system("apt install unzip -y")
+  os.system("apt install openssh -y")
+  os.system("apt install tor -y")
+  os.system("apt install net-tools -y")
+  os.system("apt install unrar -y")
+  os.system("apt install clang -y")
+  os.system("apt install w3m -y")
+  os.system("apt install proot -y")
+  os.system("pip install requests -y")
+  os.system("pip install mechanize -y")
+  os.system("pip install --upgrade pip")
+  os.system("pip2 install --upgrade pip")
+  os.system("apt install python2 -y")
+  os.system("pkg install cmatrix -y")
+  os.system("pip install future -y")
+  os.system("apt install curl -y")
+  os.system("apt install proot -y")
+  os.system("pip install youtube-dl -y")
+  os.system("pkg install weechat-python-plugin -y")
+  os.system("pkg install python2-static -y")
+  os.system("pkg install vim-python -y")
+  os.system("pkg install python-tkinter -y")
+  os.system("pkg install python-static -y")
+  print("")
+  os.system("clear")
+  time.sleep(0.2)
+  
+  print("\033[93m"+ """                                            
+
+  _________  ____  ___    _____  ______
+ / ___/ __ \/ __ \/ _ \  / _ ) \/ / __/
+/ (_ / /_/ / /_/ / // / / _  |\  / _/  
+\___/\____/\____/____/ /____/ /_/___/  
+                                       
+
+  """)
+  print ("\033[90m"+"Now Your Termux Is Ready To Run, Please exit your Termux & open again . . . . . . . ")
+  
+elif inp =="N" :
+  
+ print("\033[96m"+"GOOD BYE BRO, SEE YOU NEXT TIME")
+ exit()
